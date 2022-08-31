cc44d796530f0c3f9a0e4c442f70d2a3c5401ba1
채지우 <jw@chaejiuui-MacBookAir.local>
2022-08-31 15:40:25 +0900

    차트페이지 스크롤 오류 수정

--- src/pages/Chartpage.js
+++ src/pages/Chartpage.js
@@ -2,19 +2,15 @@ import * as React from 'react';
 import {useEffect, useState} from 'react';
 import {ScrollView, Text, View} from 'react-native';
 import Header from '../component/layout/Header';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import ChartSearch from '../component/ChartElement/ChartSearch';
 import Chart from '../component/ChartElement/Chart';
 
-
 function Chartpage({setLogin, route}) {
-
   // 여기서 route.params.name 을하면, Searchpage에서 클릭한 주식이름을 받을수 있습니다
-  // 근데 Searchpage 클릭이 아닌 그냥 BottomTab으로 최초 Chartpage 이동하게되면, 오류가납니다! 
-  
-  useEffect(()=>{
-    console.log(route.params.name)
-  },[])
+  // 근데 Searchpage 클릭이 아닌 그냥 BottomTab으로 최초 Chartpage 이동하게되면, 오류가납니다!
+
+  useEffect(() => {
+    // console.log(route.params.name);
+  }, []);
 
   return (
     <>
@@ -25,12 +21,10 @@ function Chartpage({setLogin, route}) {
           backgroundColor: 'white',
         }}>
         <Header setLogin={setLogin} pageName={'차트'} />
-        <ChartSearch contentContainerStyle={{flex: 1}} />
+        <Chart contentContainerStyle={{flex: 1}} />
       </ScrollView>
     </>
   );
 }
-
-
 
 export default Chartpage;
